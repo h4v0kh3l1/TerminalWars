d5d0bd91dcf3c83a9f139de2098eb4dccadc315b
Heli Wang <heli.h101@gmail.com>
2011-12-10 14:32:03 -0500

    Initial Commit

---
+++ TerminalWars/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TerminalWars
+//
+//  Created by Heli Wang on 12/10/11.
+//  Copyright (c) 2011 Carnegie Mellon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
